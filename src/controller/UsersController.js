2013d3a7fd111a4650f0725ac5b53c122df0538e
Brambs <gabriel128.br@gmail.com>
2023-03-08 16:29:33 -0300

    App error

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,7 +1,12 @@
+const AppError = require('../utils/AppError')
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
 
+    if (!name) {
+      throw new AppError('Nome é obrigatorio')
+    }
     response.send(`Name: ${name}, Email: ${email}`)
   }
 }
