8cb9ce4fc87c93a502bb1e0c8b7c066b527165d0
Brambs <gabriel128.br@gmail.com>
2023-04-18 15:00:27 -0300

    Middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 
 const UsersController = require('../controller/UsersController')
+const ensureAuthenticated = require('../middleware/ensureAuthenticated')
 
 const usersRoutes = Router()
 
 const userController = new UsersController()
 
 usersRoutes.post('/', userController.create)
-usersRoutes.put('/:id', userController.update)
+usersRoutes.put('/:id', ensureAuthenticated, userController.update)
 
 module.exports = usersRoutes
